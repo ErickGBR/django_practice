0560b35a4ac20e87acd315be2514292576feb375
ErickGBR <erickburgos1519@gmail.com>
2024-04-12 00:38:08 -0600

    add new task request

--- task/views.py
+++ task/views.py
@@ -1,11 +1,35 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-task = ["foo", "bar", "baz"]
+
+tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    tasks = forms.CharField(label="new task")
+    priority = forms.IntegerField(label="Priority", min_value=0, max_value=10)
+
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html",{
-        "tasks": task,
+        "tasks": tasks,
     })
 
 def add(request):
-    return render(request, "tasks/add.html",{})
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            form.cleaned_data
+            task = form.cleaned_data["tasks"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("task:index"))
+        
+        else:
+            return render(request, "task/add.html",{
+                "form": form
+            })
+        
+    return render(request, "tasks/add.html",{
+        "form": NewTaskForm()
+        })
